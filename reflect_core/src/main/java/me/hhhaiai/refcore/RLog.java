51b3d32f1317b069723cdc4c3998beb5e95c0956
sanbo <sanbo.xyz@gmail.com>
2021-05-25 14:54:16 +0800

    增加log test

--- reflect_core/src/main/java/me/hhhaiai/refcore/RLog.java
+++ reflect_core/src/main/java/me/hhhaiai/refcore/RLog.java
@@ -2,28 +2,50 @@ package me.hhhaiai.refcore;
 
 import android.util.Log;
 
-class RLog {
+public class RLog {
     private static final String TAG = "sanbo";
 
+    public static void v(Throwable igone) {
+        v(Log.getStackTraceString(igone));
+    }
+
+    public static void d(Throwable igone) {
+        d(Log.getStackTraceString(igone));
+    }
+
+    public static void i(Throwable igone) {
+        i(Log.getStackTraceString(igone));
+    }
+
     public static void e(Throwable igone) {
-        Log.e(TAG, Log.getStackTraceString(igone));
+        e(Log.getStackTraceString(igone));
     }
+
+    public static void w(Throwable igone) {
+        w(Log.getStackTraceString(igone));
+    }
+
     public static void v(String info) {
-        Log.v(TAG, info);
+        Log.println(Log.VERBOSE, TAG, info);
     }
+
     public static void d(String info) {
-        Log.d(TAG, info);
+        Log.println(Log.DEBUG, TAG, info);
     }
+
     public static void i(String info) {
-        Log.i(TAG, info);
+        Log.println(Log.INFO, TAG, info);
     }
+
     public static void w(String info) {
-        Log.w(TAG, info);
+        Log.println(Log.WARN, TAG, info);
     }
+
     public static void e(String info) {
-        Log.e(TAG, info);
+        Log.println(Log.ERROR, TAG, info);
     }
+
     public static void wtf(String info) {
-        Log.wtf(TAG, info);
+        Log.println(Log.ASSERT, TAG, info);
     }
 }
