d88e7c35b6a3795f9b74e9f7792e7d87bc565de6
sanbo <sanbo.xyz@gmail.com>
2022-04-29 15:20:23 +0800

    update sth. at 2022-04-29 15:20:20

--- Java/src/test/java/me/demo/tess/PublicB.java
+++ Java/src/test/java/me/demo/tess/PublicB.java
@@ -2,6 +2,7 @@ package me.demo.tess;
 
 public class PublicB {
     String name = "q";
+    static String stati_Name = "Twodog";
 
     public PublicB() {
         name += "q";
@@ -10,8 +11,13 @@ public class PublicB {
     public String getB() {
         return name;
     }
+
     public String getSss() {
         return "22222";
+    }
+
+    public static String getSome() {
+        return "static args:" + stati_Name + "\r\n" + "not static cann't read！" ;
     }
 
     public static String getA() {
